a44f2d2e99e7580f87deab0ef3f666067fec4fcf
Takanuva <lafrite@poneyworld.net>
2012-10-17 16:27:19 +0200

    Modify \n otherwise it didn't work

--- bots/Imitator_php/bot.php
+++ bots/Imitator_php/bot.php
@@ -2,20 +2,20 @@
 print("OK\n");
 
 # Ma première réponse
-$ans = "C";
+$ans = "C\n";
 
 # Réponse suposé de l'autre
-$other = "C";
+$other = "C\n";
 
 # On attends le message de départ (qui doit être OK)
 $master = fgets(STDIN);
 
 # Tant qu'on ne nous a pas dit qu'on arretait
-while($master != "Q"){
+while($master != "Q\n"){
     # On copie la réponse de l'autre
     $ans = $other;
     # On envoie notre réponse
-    print($ans . "\n");
+    print($ans);
     # On reçoit la réponse de l'autre
     $other = fgets(STDIN);
     # On attend un nouveau message du maitre (Continuer ou stop)
