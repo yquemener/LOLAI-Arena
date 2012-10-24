9e59549c7704f3ce3517bbdacdb904dcb9a16515
Yves Quemener <Iv@laptop>
2012-10-24 15:33:16 +1100

    Added a bot

--- bots/IvBot/bot.py
+++ bots/IvBot/bot.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python2
 #-*- coding:utf8-*-  
-from random import *
-from time import *
+
+# This bot is an imitator that begins with cooperatio, betrays on the last
+# and betrays any partner who betrayed more than thress times.
+
 import sys
 
-seed(time())
-
+rounds=1
+totalT=0
 print "OK"
-received=raw_input()
+keeprunning=raw_input()
+
 ans="C"
-while received!='Q':
-    print ans
-    received=raw_input()
-    ans=received
+while keeprunning!='Q':
+
+    if rounds==50 or totalT>3:
+        print 'T'
+    else:
+        print ans
+    ans=raw_input()
+    keeprunning=raw_input()
+    if(ans=='T'): totalT+=1
+    rounds+=1
