e01e8e0c90b75e294f586009d7adf46024a1908d
dadadel <daouzli@gmail.com>
2012-11-13 23:22:39 +0100

    Update docstrings

--- Games/Prisonnier/prisonnier.py
+++ Games/Prisonnier/prisonnier.py
@@ -11,21 +11,18 @@ ROUND_TIMEOUT = 0.01
 
 class Prisonnier(Game):
     NAME = "Prisonnier"
-    def __init__(self, bots, round = 50):
-        """ Initialization of the prisonnier
+    def __init__(self, bots, round=50):
+        """Initialization of the prisonnier
 
         @param bots: list of bots
         @param round: number of round
 
         """
-
         Game.__init__(self, Prisonnier.NAME, bots)
         self.round = int(round)
 
     def steady_bots(self):
-        """Description of steady_bots
-
-        Send to bots the new round message
+        """Sends to bots the new round message
     
         """
         for b in self.bots:
@@ -35,7 +32,9 @@ class Prisonnier(Game):
     # Main step of the game - adapted to prisonnier
 
     def run_game(self):
-        """ Process of the game """
+        """ Process of the game 
+        
+        """
         for k in range(self.round):
             # Send to bots steady message
             self.steady_bots()
@@ -43,9 +42,7 @@ class Prisonnier(Game):
             self.go()
 
     def go(self):
-        """Description of go
-
-        Rules of the game
+        """Rules of the game
     
         """
         # Reading bots choices
@@ -53,41 +50,48 @@ class Prisonnier(Game):
         r2 = self.bots[1].get_ans()
 
         # Distribute points
-        if r1=="C" and r2=="C":
-            self.bots[0].score+=5
-            self.bots[1].score+=5
-        elif r1=="C" and r2=="T":
-            self.bots[0].score+=0
-            self.bots[1].score+=10
-        elif r1=="T" and r2=="C":
-            self.bots[0].score+=10
-            self.bots[1].score+=0
-        elif r1=="T" and r2=="T":
-            self.bots[0].score+=1
-            self.bots[1].score+=1
+        if r1 == "C" and r2 == "C":
+            self.bots[0].score += 5
+            self.bots[1].score += 5
+        elif r1 == "C" and r2 == "T":
+            self.bots[0].score += 0
+            self.bots[1].score += 10
+        elif r1 == "T" and r2 == "C":
+            self.bots[0].score += 10
+            self.bots[1].score += 0
+        elif r1 == "T" and r2 == "T":
+            self.bots[0].score += 1
+            self.bots[1].score += 1
         else:
-            raise ValueError("Your answer doesn't correspond to the game:  r1= {r1}, r2 = {r2}".format(r1 = r1, r2 = r2))
+            raise ValueError("Your answer doesn't correspond to the game:  r1 = {r1}, r2 = {r2}".format(r1=r1, r2=r2))
 
         # Send results to other bots
-        self.bots[0].send_msg(r2+'\n')
-        self.bots[1].send_msg(r1+'\n')
+        self.bots[0].send_msg(r2 + '\n')
+        self.bots[1].send_msg(r1 + '\n')
 
 
     def det_winner(self):
-        """ Choose the winner """
+        """ Choose the winner 
+        
+        """
         # On doit pouvoir faire mieux pour récuperer l'index du gagnant avec un l.index(max(l)).
         # Mais ya un soucis avec le cas d'égalité
         if (self.bots[1].score > self.bots[0].score):
-            self.winner = "{winner} wins!".format(winner = self.bots[1].name)
+            self.winner = "{winner} wins!".format(winner=self.bots[1].name)
         elif (self.bots[1].score < self.bots[0].score):
-            self.winner = "{winner} wins!".format(winner = self.bots[0].name)
+            self.winner = "{winner} wins!".format(winner=self.bots[0].name)
         else:
             self.winner = "Draw"
 
     def give_results(self):
-        """ Give the sum up of the game"""
+        """Give the sum up of the gam
+        
+        @return: the bots and the winner
+        @rtype: dictionary
+        
+        """
         self.det_winner()
-        return {'bots' : self.bots, 'winner' : self.winner}
+        return {'bots': self.bots, 'winner': self.winner}
 
 
     # -------------------
@@ -95,18 +99,18 @@ class Prisonnier(Game):
 
     @classmethod
     def requiered_info(self):
-        """ Docstring for requiered_info
+        """Gets requiered informations for the game such as
+        number of players, number of rounds....
         
-        Return requiered informations for the game such number of players, number of rounds....
+        @return: number of players and of rounds
+        @rtype: dictionary
         
         """
-        return {"players":2, "round": (50, "Nombre de manches")}
+        return {"players": 2, "round": (50, "Nombre de manches")}
 
     @classmethod
     def rules(self):
-        """ Docstring for rules
-        
-        Return the rules of the game
+        """Gets the rules of the game
         
         """
         pass
