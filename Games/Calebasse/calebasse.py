96a37c5dd4d167e98e8b174bbd2c76ea2e2b80dd
Takanuva <lafrite@poneyworld.net>
2012-11-25 17:17:55 +0100

    Protocole for bots and starting working on the game

--- Games/Calebasse/calebasse.py
+++ Games/Calebasse/calebasse.py
@@ -4,10 +4,24 @@
 # ------------------------------
 # Imports
 # ------------------------------ 
+from game import Game
+from bot import Bot
+
+from time import *
+
+ROUND_TIMEOUT = 0.01
 
 # ------------------------------
 # Classes
 # ------------------------------ 
+
+class Calebasse(Game):
+    NAME = "Calebasse"
+    def __init__(self, arg):
+        """ Initiate Calebasse """
+        super(Calebasse, self).__init__()
+        self.arg = arg
+        
 
 # ------------------------------
 # Fonctions
