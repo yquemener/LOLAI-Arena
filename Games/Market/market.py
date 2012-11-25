226b23fa7985a257f17fa391ef10804368478f24
Yves Quemener <Iv@laptop>
2012-11-25 19:35:48 +1100

    Implementing the market game

--- Games/Market/market.py
+++ Games/Market/market.py
@@ -6,7 +6,25 @@ from bot import Bot
 
 from time import *
 
+import json
+
 ROUND_TIMEOUT = 0.01
+
+class Player:
+    def __init__(self, name):
+        self.cash = 100
+        self.farms = 0
+        self.mills = 0
+        self.wheat = 0
+        self.flour = 0
+        self.name = name
+
+    def state(self):
+        return [self.name, self.cash,
+                self.wheat,
+                self.flour,
+                self.farms,
+                self.mills]
 
 
 class Market(Game):
@@ -19,7 +37,31 @@ class Market(Game):
 
         """
         Game.__init__(self, Market.NAME, bots)
+
+        # Initialization of the constants of the game
+        self.farm_price = 20
+        self.mill_price = 85
+        self.transformation_rate = 1.0
+        self.growing_cycle = 100
+        self.flour_bought_each_turn = 50
+        self.farm_production = 1
+        self.mill_transformation = 1
+
+        # Initialization of the markets
+        self.wheat_martket=list()
+        self.flour_market=list()
+        self.transactions_done=list()
+        self.stats=list()
+
         self.round = int(round)
+        self.players_state = dict()
+        self.bots = dict()
+        i = 0
+        for b in bots:
+            botname = b.name+str(i)
+            self.bots[botname] = b
+            self.players_state[botname]=Player(botname)
+            i+=1
 
     def steady_bots(self):
         """Sends to bots the new round message
@@ -35,8 +77,12 @@ class Market(Game):
         """ Process of the game 
         
         """
+        # send its bot its identification
+        for botname in self.bots.keys():
+            self.bots[botname].send_msg(botname)
+
         for k in range(self.round):
-            # Send to bots steady message
+            # Send to bots the state of the world 
             self.steady_bots()
             # Rules of the game
             self.go()
@@ -46,12 +92,35 @@ class Market(Game):
     
         """
         # Reading bots choices
-        r1 = self.bots[0].get_ans()
-        r2 = self.bots[1].get_ans()
+        for bn in self.bots.keys():
+            ans=json.loads(self.bots[bn].get_ans())
+            for act in ans:
+                if len(act)==3:
+                    # farm/mill buy
+                    if act[0]=="buy":
+                        self.buy_facility(bn,act[1], act[2])
+                    else:
+                        self.sell_facility(bn,act[1], act[2])
+                if len(act)==4:
+                    # flour/wheat transaction
+                    if act[1]=="flour":
+                        self.flour_market.append(
+                                [bn,act[0],act[2],act[3]])
+                    else:
+                        self.wheat_market.append(
+                                [bn,act[0],act[2],act[3]])
 
-        # Send results to other bots
-        self.bots[0].send_msg(r2 + '\n')
-        self.bots[1].send_msg(r1 + '\n')
+    def buy_facility(self, botname, ftype, qty):
+        if ftype=="farm":
+            if(self.players_state[botname].cash >=
+               qty*self.farm_price):
+                self.players_state[botname].cash-=qty*self.farm_price
+                self.players_state[botname].farms+=qty
+        if ftype=="mill":
+            if(self.players_state[botname].cash >=
+               qty*self.mill_price):
+                self.players_state[botname].cash-=qty*self.mill_price
+                self.players_state[botname].mill+=qty
 
 
     def det_winner(self):
